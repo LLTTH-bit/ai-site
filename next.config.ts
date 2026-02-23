550b4c162ce1dc03e2344b1618806fd4e77723b6
LLTTH <cuber936bit@163.com>
2026-02-24 02:41:08 +0800

    fix: next.config添加photos配置

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
         source: "/uploads/:path*",
         destination: "/uploads/:path*",
+      },
+      {
+        source: "/photos/:path*",
+        destination: "/photos/:path*",
       },
     ];
   },
